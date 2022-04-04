ef7dbcdd6647a5604859af1df0423fe0e3694316
misterhoughton <misterhoughton@users.noreply.github.com>
2022-04-04 12:02:31 +0100

    Initial commit

---
+++ run.py
@@ -0,0 +1,180 @@
+from random import randint
+
+# Legend:
+EMPTY = 0
+SHIP = 1
+HIT = 2
+MISS = 3
+
+def getBoard(boardSize):
+    board = []
+    for i in range(boardSize):
+        board.append([EMPTY] * boardSize)
+    return board
+
+# Output the raw board to the console:
+def printBoard(board):
+    for row in board:
+        rowStr = ""
+        for cell in row:
+            cellStr = str(cell)
+            rowStr += cellStr
+        print(rowStr)
+
+# Output the board to the console with row and column indexes and hidden ships:
+def printPlayBoard(board):
+    rowIndex = " "
+    rowBorder = "-"
+    colIndex = 0
+    
+    for i in range(len(board[0])):
+        rowIndex = rowIndex + " " + str(i)
+        rowBorder += "--"
+
+    print(rowIndex)
+    print(rowBorder)
+
+    for row in board:
+        rowStr = str(colIndex) + "|"
+        colIndex += 1
+        for cell in row:
+            if(cell == HIT):
+                cellStr = "* "
+            else:
+                if(cell == MISS):
+                    cellStr = "X "
+                else:
+                    cellStr = "O "
+            rowStr += cellStr
+
+        print(rowStr)
+    
+    print("\n")
+
+def getNumberOfShips(board):
+    numberOfShips = 0
+    for row in board:
+        for cell in row:
+            if(cell == SHIP):
+                numberOfShips += 1
+    return numberOfShips
+
+def getRandomCoords(board):
+    x = randint(0, len(board) - 1)
+    y = randint(0, len(board) - 1)
+    return x, y
+
+# Create ships in random locations:
+def addShips(board, numberOfShips):
+    for i in range(numberOfShips):
+        shipRow, shipColumn = getRandomCoords(board)
+        setValue(board, shipRow, shipColumn, SHIP)
+
+# Returns the value at the provided co-ordinates:
+def getValue(board, x, y):
+    return board[y][x]
+
+# Sets the value at the co-ordinates of the board:
+def setValue(board, x, y, value):
+    board[y][x] = value
+
+# Validate user input:
+def validate(board, x, y):
+    validRange = range(len(board[0]))
+    try:
+        x = int(x)
+        y = int(y)
+        if(x not in validRange or y not in validRange):
+            print("Out of range!")
+            return False
+        else:
+            value = getValue(board, x, y)
+            if(value > SHIP):
+                print("You've tried those co-ordinates already!")
+                return False
+            else:
+                return True
+    except ValueError:
+        print("Please use valid co-ordinates")
+        return False
+
+def takeTurn(board):
+    printPlayBoard(board)
+    x = raw_input("Enter co-ordinate X: ")
+    y = raw_input("Enter co-ordinate Y: ")
+
+    isValid = validate(board, x, y)
+
+    if(isValid):
+        x = int(x)
+        y = int(y)
+        value = getValue(board, x, y)
+        print("\n")
+        if(value == SHIP):
+            setValue(board, x, y, HIT)
+            print("* * * * * * * *")
+            print("* Direct hit! *")
+            print("* * * * * * * *")
+
+        else:
+            print("Sorry, you missed")
+            setValue(board, x, y, MISS)
+        return True
+        
+    return False
+
+# Play a full game. Returns 1 for a win, 0 for a loss
+def playGame():
+    BOARD_SIZE = 3
+    NUMBER_OF_SHIPS = 3
+    BOARD = getBoard(BOARD_SIZE)
+
+    addShips(BOARD, NUMBER_OF_SHIPS)
+    numberOfTurns = 5
+
+    while(getNumberOfShips(BOARD) > 0 and numberOfTurns > 0):
+        status = str(getNumberOfShips(BOARD)) + " ships left. "
+        status += str(numberOfTurns) + " turns left"
+        print(status)
+        print("\n")
+
+        if(takeTurn(BOARD)):
+            numberOfTurns -= 1
+
+
+
+    printPlayBoard(BOARD)
+
+    if(getNumberOfShips(BOARD) == 0):
+        print("Well done, all ships destroyed")
+        return 1
+    else:
+        print("Sorry, you have no turns left")
+        return 0
+
+def gameMenu():
+    validValues = ["1", "2", "3", "4"]
+    score = 0
+    option = ""
+    while(option != "4"):
+        print("*******************")
+        print("1 - Instructions")
+        print("2 - Play game")
+        print("3 - Your score")
+        print("4 - Quit")
+        print("*******************")
+        option = raw_input("Please enter your choice:  ")
+        if (option in validValues):
+            if option == "1":
+                print("show instructions")
+            if option == "2":
+                score += playGame()
+            if option == "3":
+                print("Your score is: " + str(score))
+            if option == "4":
+                print("Goodbye!")
+        else:
+            print("Please select a valid number 1, 2, 3 or 4")
+
+if __name__ == "__main__":
+    gameMenu()
